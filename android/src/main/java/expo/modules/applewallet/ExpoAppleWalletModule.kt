d7a227293b2c0f905dd7b035e1a86fe22055df5d
vitoralvesdev <vitorhugoalvees@gmail.com>
2025-06-27 08:02:25 -0300

    fix: resolved conflict that breaks code on android even without using lib

---
+++ android/src/main/java/expo/modules/applewallet/ExpoAppleWalletModule.kt
@@ -0,0 +1,11 @@
+package expo.modules.applewallet
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class ExpoAppleWalletModule : Module() {
+  override fun definition() = ModuleDefinition {
+    Name("ExpoAppleWallet")
+
+  }
+}
